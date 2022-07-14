516608328edba66831edee3e309edb0c3728b023
LaibaTanveer <laibatanveer@A006-00218.local>
2022-07-14 18:25:23 +0500

    Updated code according to pull request

--- typer-task.py
+++ typer-task.py
@@ -1,13 +1,20 @@
 import typer
 app =  typer.Typer()
+# num_of_line=4
+x=int(input("Enter number to print lines "))
 
 @app.command()
 def writeTo(file):
     try:
         with open(file, "r+") as f:
-            print(f.read())
+            counter = 1
+            for line in f:
+                counter = counter+1
+                print(f.readline())
+                if counter>x:
+                    break
     except TypeError:
-        print("File not Found")
+        print(TypeError)
 
 if __name__ == "__main__":
     app()
